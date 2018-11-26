e12b835c2cca0d24cad17f714577d6001fdad106
zouzouxinxian <zouzouxinxian@outlook.com>
2018-11-26 23:23:44 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//假设main.js还是入口文件
+
+console.log('succes!');
+
+
+//webpack-dev-server 打包好的main.js 是托管到了内存中，所以在项目根目录中看不到
+//但是，可以认为，在项目根目录中，有一个看不见的main.js
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+//向外暴露一个打包的配置对象；
+//因为webpack是基于node构建的，所以webpack支持所有的Node API 和语法
+
+module.exports={
+    mode:'development', //development   production
+
+    //在webpack 4.x 中，有一个很大的特性，就是约定大于配置，约定默认的路径是src ->  index.js
+
+}
+
+
+
+//es6中向外导出模块的API 与之对应的是 import **  form  **
+
+
+//export defa额u过ult{}
+
+//哪些特性Node支持？
+// 如果chorme 浏览器支持哪些，则Node 就支持哪些
